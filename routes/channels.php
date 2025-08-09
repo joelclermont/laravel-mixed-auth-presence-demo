9c6bbb9c55051a6fedde201ea0cef6f1ee60735f
Joel Clermont <joel@nocompromises.io>
2025-08-09 12:20:58 -0500

    set up mixed auth presence channel

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('mixed-auth-presence-demo', function ($user = null) {
+    if ($user) {
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'type' => 'authenticated',
+        ];
+    }
+    
+    $guestId = request()->session()->get('guest_id');
+    $guestName = request()->session()->get('guest_name');
+    
+    if ($guestId) {
+        return [
+            'id' => $guestId,
+            'name' => $guestName,
+            'type' => 'guest',
+        ];
+    }
+    
+    return false;
+});
